02864385aa93bea43de853194533ab3f51b6fe60
Shade1912 <pushkarsave1912@gmail.com>
2019-07-28 20:47:33 +0530

    Cart
    Added cart without proper alignment(not complete)

---
+++ JavaScript/custom_main.js
@@ -0,0 +1,11 @@
+function showCart()
+{
+    document.getElementById('cart').style.display='block';
+    document.getElementById('fbutton').style.display='none';
+}
+
+function hideCart()
+{
+    document.getElementById('cart').style.display='block';
+    document.getElementById('fbutton').style.display='none';
+}
